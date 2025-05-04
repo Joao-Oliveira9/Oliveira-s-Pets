842f99fd70abbd6c3907c9618ab939b39db122b1
João Oliveira <joaoliveira9@outlook.com>
2025-05-04 17:37:06 -0300

    Update: salvando um animal, repository e service

---
+++ src/main/java/com/example/demo/core/domain/entities/Animal.java
@@ -0,0 +1,108 @@
+package com.example.demo.core.domain.entities;
+
+import jakarta.persistence.*;
+import org.hibernate.annotations.CreationTimestamp;
+import org.hibernate.annotations.UpdateTimestamp;
+import org.springframework.boot.autoconfigure.web.WebProperties;
+
+import java.time.LocalDateTime;
+import java.util.UUID;
+@Entity
+@Table(name = "TB_ANIMAL")
+public class Animal {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.UUID)
+    private UUID id;
+
+    @Column(name = "nome_do_animal",nullable = false)
+    private String nome;
+
+    @Column(name="idade",nullable = false)
+    private String idade;
+
+    @Column(name = "tipo",nullable = false)
+    private String tipo;
+
+    @Column(name = "porte",nullable = false)
+    private String porte;
+
+    @Column(name = "cpf",nullable = false)
+    private String cpf;
+
+    @CreationTimestamp
+    @Column(name = "created_at",updatable = false)
+    private LocalDateTime created_at;
+
+    @UpdateTimestamp
+    @Column(name = "updated_at")
+    private LocalDateTime update_at;
+
+    public LocalDateTime getUpdate_at() {
+        return update_at;
+    }
+
+    public void setUpdate_at(LocalDateTime update_at) {
+        this.update_at = update_at;
+    }
+
+    public LocalDateTime getCreated_at() {
+        return created_at;
+    }
+
+    public void setCreated_at(LocalDateTime created_at) {
+        this.created_at = created_at;
+    }
+
+    public UUID getId() {
+        return id;
+    }
+
+    public void setId(UUID id) {
+        this.id = id;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getTipo() {
+        return tipo;
+    }
+
+    public void setTipo(String tipo) {
+        this.tipo = tipo;
+    }
+
+    public String getPorte() {
+        return porte;
+    }
+
+    public void setPorte(String porte) {
+        this.porte = porte;
+    }
+
+    public String getCpf() {
+        return cpf;
+    }
+
+    public void setCpf(String cpf) {
+        this.cpf = cpf;
+    }
+
+    public String getIdade() {
+        return idade;
+    }
+
+    public void setIdade(String idade) {
+        this.idade = idade;
+    }
+}
+
+
+
+
---
+++ src/main/java/com/example/demo/core/service/RegistroAnimalService.java
@@ -0,0 +1,30 @@
+package com.example.demo.core.service;
+
+import com.example.demo.core.domain.entities.Animal;
+import com.example.demo.core.domain.usecase.RegistroDeAnimalUsecase;
+import com.example.demo.infra.port.AnimalRepository;
+import com.example.demo.presenters.Dtos.AnimalDto;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
+public class RegistroAnimalService implements RegistroDeAnimalUsecase {
+
+    @Autowired
+    AnimalRepository animalRepository;
+
+    public void registroAnimal(AnimalDto animalDto){
+        Animal animal = new Animal();
+
+        animal.setCpf(animalDto.cpf_dono());
+        animal.setIdade(animalDto.idade());
+        animal.setPorte(animalDto.porte());
+        animal.setNome(animalDto.nome_do_animal());
+        animal.setTipo(animalDto.tipo());
+
+        animalRepository.save(animal);
+    }
+    public void existeAnimal(){
+
+    }
+}
